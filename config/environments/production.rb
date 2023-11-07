ab4f5ef1ad51c6df30dfaad256ecb7e4ee34798f
nellotte <nelly.guerin@gmail.com>
2023-11-07 19:20:51 +0100

    Fix production.rb configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 require "active_support/core_ext/integer/time"
 
-config.action_mailer.default_url_options = { :host => 'devise-eventbright2.herokuapp.com' }
+
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -96,4 +96,6 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_mailer.default_url_options = { :host => 'devise-eventbright2.herokuapp.com' }
 end
